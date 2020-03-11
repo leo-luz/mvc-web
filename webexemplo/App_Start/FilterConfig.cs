3354b18ab43c9f2736be793e8f68b654857dff57
leonardocharles <leonardocharles@outlook.com>
2020-03-11 17:17:49 -0300

    Primeiro Commit

---
+++ webexemplo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace webexemplo
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ webexemplo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="webexemplo.MvcApplication" Language="C#" %>
